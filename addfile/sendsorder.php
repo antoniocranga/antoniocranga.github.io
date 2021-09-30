9e7f89a97fe9a56eb0592f3b2126264a4f5467df
Antonio <antocranga@gmail.com>
2021-09-30 13:15:43 +0300

    Update

--- addfile/sendsorder.php
+++ addfile/sendsorder.php
@@ -2,14 +2,14 @@
 echo "entered"
    $subject = $_POST['Budget']. ' ' . $_POST['Email']
    $res = mail(
-      "office.uiglobal@gmail.com",
+      "antocranga@gmail.com",
       $subject,
       $_POST['Message']
    );
-   echo $_POST['Email'] . ' ' . $_POST['Email'] . ' ' . $_POST['Message'];
+
    if($res == false)
       echo "failed"
-   header('Location: /emailsent.php');
+   header('Location: /addfile/emailsent.php');
    exit();
 ?>
 <html> </html>
